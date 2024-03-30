f995445eadcc94849095393ed6cbc7aa3554e9d5
Zeke Zhang <sean1832725142@gmail.com>
2024-03-31 03:08:00 +1100

    feat: display downloaded item dispite verbose is disabled

--- pinterest_cli/downloader.py
+++ pinterest_cli/downloader.py
@@ -15,8 +15,7 @@ def download(url: str, output_dir, chunk_size=2048, verbose=False):
         with open(outfile, "wb") as payload:
             for chunk in req.iter_content(chunk_size):
                 payload.write(chunk)
-        if verbose:
-            print(f"Downloaded {filename}")
+        print(f"Downloaded {filename}")
         return outfile
     else:
         print("URL must be a string.")
