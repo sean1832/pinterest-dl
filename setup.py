a64f23a87b35c1db99d52089c59d6a4f41f2e62e
Zeke Zhang <sean1832725142@gmail.com>
2024-03-31 01:58:59 +1100

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ import json
 from setuptools import find_packages, setup
 
 manifest = json.load(open("pinterest_cli/manifest.json", "r"))
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name=manifest["name"],
     version=manifest["version"],
     author=manifest["author"],
     description=manifest["description"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=manifest["license"],
     url=manifest["url"],
     packages=find_packages(),
     include_package_data=True,
@@ -17,10 +22,24 @@ setup(
             "manifest.json",
         ]
     },
-    install_requires=["selenium"],
+    install_requires=["selenium>=4.9.0"],
     entry_points={
         "console_scripts": [
             "pinterest-cli = pinterest_cli.main:main",
         ],
     },
+    python_rquires=">=3.10",
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Environment :: Console",
+        "Operating System :: Microsoft :: Windows",
+    ],
 )
