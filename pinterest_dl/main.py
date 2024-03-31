37f9d08db811d83d182f756e137a18bedfb068fa
Zeke Zhang <sean1832725142@gmail.com>
2024-04-01 04:46:55 +1100

    feat: create api abstraction for easier use as library

--- pinterest_dl/main.py
+++ pinterest_dl/main.py
@@ -1,59 +1,4 @@
-from pinterest_dl import cli_parser, downloader, io, scraper, utils
-
-
-def run_download(img_urls, output, verbose):
-    fallback_urls = [i.replace("/originals/", "/736x/") for i in img_urls]
-    return downloader.download_concurrent_with_fallback(
-        img_urls, output, fallback_urls, verbose=verbose
-    )
-
-
-def run_prune(local_images, min_resolution):
-    if min_resolution:
-        for i in local_images:
-            utils.prune_by_resolution(i, min_resolution)
-
-
-def run_scrape(
-    url,
-    threshold,
-    firefox,
-    output,
-    write,
-    persistence,
-    incognito,
-    dry_run,
-    verbose,
-    min_resolution,
-):
-    browser = scraper.Browser().Chrome(
-        exe_path=utils.get_appdata_dir("chromedriver.exe"),
-        incognito=incognito,
-    )
-    if firefox:
-        browser = scraper.Browser().Firefox()
-    try:
-        pin_scraper = scraper.Pinterest(browser)
-        img_urls = pin_scraper.scrape(
-            url,
-            threshold=threshold,
-            presistence=persistence,
-            verbose=verbose,
-        )
-    finally:
-        browser.close()
-
-    print(f"Found {len(img_urls)} urls")
-    if write:
-        io.write_json(img_urls, write, indent=4)
-    if not dry_run:
-        downloaded_files = run_download(img_urls, output, verbose)
-
-        # post download
-        run_prune(downloaded_files, min_resolution)
-    else:
-        for i in img_urls:
-            print(i)
+from pinterest_dl import api, cli_parser, io
 
 
 def main():
@@ -61,7 +6,7 @@ def main():
     args = parser.parse_args()
 
     if args.cmd == "scrape":
-        run_scrape(
+        api.run_scrape(
             args.url,
             args.threshold,
             args.firefox,
@@ -76,8 +21,8 @@ def main():
         print("\nDone.")
     elif args.cmd == "download":
         img_list = io.read_json(args.url_list)
-        downloaded_files = run_download(img_list, args.output, args.verbose)
-        run_prune(downloaded_files, args.resolution)
+        downloaded_files = api.run_download(img_list, args.output, args.verbose)
+        api.run_prune(downloaded_files, args.resolution)
         print("\nDone.")
     else:
         parser.print_help()
