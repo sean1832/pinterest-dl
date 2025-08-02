35f73c052226dd230f47fb5b8c534fb90a4c24ae
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2025-08-03 00:20:50 +1000

    feat: add download_blob method to HttpClient for downloading content

--- pinterest_dl/low_level/http/http_client.py
+++ pinterest_dl/low_level/http/http_client.py
@@ -1,4 +1,5 @@
-from typing import Iterable
+from pathlib import Path
+from typing import Union
 
 import requests
 import requests.adapters
@@ -46,4 +47,19 @@ class HttpClient:
         resp = self.session.get(url, timeout=self.timeout, **kwargs)
         resp.raise_for_status()
         return resp
-    
+
+    def download_blob(
+        self, url: str, output_path: Union[str, Path], chunk_size: int = 8192, **kwargs
+    ) -> None:
+        """Downloads the blob content from the specified URL and saves it to the output path.
+
+        Args:
+            url (str): The URL to download content from.
+            output_path (Union[str, Path]): The file path where the content will be saved.
+            chunk_size (int): Size of each chunk to read from the response. Defaults to 8192.
+            **kwargs: Additional keyword arguments to be passed to the request.
+        """
+        with self.get(url, stream=True, **kwargs) as response:
+            with open(output_path, "wb") as file:
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    file.write(chunk)
